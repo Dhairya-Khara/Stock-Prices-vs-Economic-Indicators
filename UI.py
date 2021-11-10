2113b841ee6ce4c218443d54fe39c1fd922a939d
Dhairya Khara <kharadhairya@gmail.com>
2021-11-10 17:20:38 -0500

    merged code manually

--- UI.py
+++ UI.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 # Note the matplot tk canvas import
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from graph1 import return_info_to_graph
+from graph3 import return_unemployed_per_opening_to_graph
+from CCI import return_info_to_graph_cci
 
 
 # Mat Plot Lib, get x and y values from graph1.py
@@ -18,6 +20,31 @@ def get_fig(stock_ticker):
     m, b = np.polyfit(x, y, 1)
     plt.plot(x, m * x + b)
     return fig
+
+
+# Mat Plot Lib, get x and y values from graph1.py
+def get_fig_2(stock_ticker):
+    p, q = return_info_to_graph_cci(stock_ticker)
+    fig_2 = plt.figure()
+    plt.title(stock_ticker + '\'s Median Monthly Stock Price vs CCI ')
+    plt.plot(p, q, 'o')
+    plt.xlabel("CCI")
+    plt.ylabel("Median Monthly Stock Price")
+    m, b = np.polyfit(p, q, 1)
+    plt.plot(p, m * p + b)
+    return fig_2
+
+
+def get_fig3(stock_ticker):
+    x, y = return_unemployed_per_opening_to_graph(stock_ticker)
+    fig3 = plt.figure()
+    plt.title(stock_ticker + '\'s Median Monthly Stock Price vs Unemployed per Job Opening Ratio')
+    plt.plot(x, y, 'o')
+    plt.xlabel("Unemployed per Job Opening")
+    plt.ylabel("Median Monthly Stock Price")
+    m, b = np.polyfit(x, y, 1)
+    plt.plot(x, m * x + b)
+    return fig3
 
 
 # Function to draw graph
@@ -35,7 +62,9 @@ figure_x, figure_y, figure_w, figure_h = get_fig("AAPL").bbox.bounds
 # define the window layout, the third element is the actual graph
 layout = [[sg.Text('Enter Stock Ticker', font='Any 18')],
           [sg.Input(key='-INPUT-'), sg.Button('Search')],
-          [sg.Canvas(size=(figure_w, figure_h), key='-CANVAS-')]]
+          [sg.Canvas(size=(figure_w, figure_h), key='-CANVAS-')],
+          [sg.Canvas(size=(figure_w, figure_h), key='-CANVAS1-')],
+          [sg.Canvas(size=(figure_w, figure_h), key='-CANVAS2-')]]
 
 # logic for window
 window = sg.Window('Stock Correlation',
@@ -43,6 +72,8 @@ window = sg.Window('Stock Correlation',
 
 # add the plot to the window
 fig_photo = draw_figure(window['-CANVAS-'].TKCanvas, get_fig("AAPL"))
+fig_photo_2 = draw_figure(window['-CANVAS2-'].TKCanvas, get_fig_2("AAPL"))
+fig_photo3 = draw_figure(window['-CANVAS1-'].TKCanvas, get_fig3("AAPL"))
 
 while True:
     # show it all again and get buttons
@@ -51,4 +82,8 @@ while True:
         break
     if event == 'Search':
         fig_photo.get_tk_widget().forget()
+        fig_photo_2.get_tk_widget().forget()
+        fig_photo3.get_tk_widget().forget()
         fig_photo = draw_figure(window['-CANVAS-'].TKCanvas, get_fig(values['-INPUT-']))
+        fig_photo_2 = draw_figure(window['-CANVAS-'].TKCanvas, get_fig(values['-INPUT-']))
+        fig_photo3 = draw_figure(window['-CANVAS1-'].TKCanvas, get_fig3(values['-INPUT-']))
---
+++ graph3.py
@@ -0,0 +1,105 @@
+"""Graph 3"""
+"""Graphs"""
+"""Stock Data"""
+import time
+import datetime
+import pandas as pd
+from pprint import pprint
+import statistics
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# STEP 1 - Get Stock Data from April 1 to Aug 30
+def get_stock_data(ticker):
+    period1 = int(time.mktime(datetime.datetime(2020, 4, 1, 0, 0).timetuple()))
+    period2 = int(time.mktime(datetime.datetime(2021, 8, 30, 23, 59).timetuple()))
+    interval = '1d'
+
+    query_string = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&' \
+                   'interval={interval}&events=history&includeAdjustedClose=true'
+
+    return pd.read_csv(query_string)
+
+
+# STEP 2 - Parse DataFrame object, i.e. make a list where each element is a tuple, first index is the date,
+# second index is the closing price
+def get_datetime_to_closing_prices(stock_info):
+    list_so_far = []
+    for index, row in stock_info.iterrows():
+        datetime_obj = datetime.datetime.strptime(row['Date'], '%Y-%m-%d')
+        list_so_far.append((datetime_obj, row['Close']))
+    return list_so_far
+
+
+# STEP 3 - Make a 2D List (list of lists) s.t. every element is a list of all the closing prices of a month
+def get_monthly_closing_prices(startingMonth, datetime_to_closing_prices):
+    current_month = startingMonth
+    temp_list = []
+    list_so_far = []
+    for day in datetime_to_closing_prices:
+        if day[0].month == current_month:
+            temp_list.append(day[1])
+        else:
+            list_so_far.append(temp_list)
+            current_month = day[0].month
+            temp_list = []
+    list_so_far.append(temp_list)
+    return list_so_far
+
+
+# STEP 4 - Calculate Median for each month
+def calculate_median_values(monthly_closing_prices):
+    list_so_far = []
+    for i in range(len(monthly_closing_prices)):
+        list_so_far.append(statistics.median(monthly_closing_prices[i]))
+    return list_so_far
+
+# Step 5 (2) - Get independent variables in a list, unemployed_per_opening in this case.
+def get_list_of_unemployed_per_opening():
+    list_so_far = []
+    adf = pd.read_excel('unemployed_per_opening.xlsx', sheet_name='Sheet 1')
+
+    for index, row in adf.iterrows():
+        list_so_far.append(row['Total'])
+
+    return list_so_far
+
+
+# STEP 6 - Linear Regression.
+def get_r_squared():
+    correlation_matrix = np.corrcoef(x, y)
+    correlation_xy = correlation_matrix[0, 1]
+    r_squared = correlation_xy ** 2
+    return r_squared
+
+
+# STEP 7 - Plot the Graph
+# def plot_the_graph():
+#     plt.plot(x, y, 'o')
+#     plt.xlabel("Unemployment Rate")
+#     plt.ylabel("Median Stock Price")
+#     m, b = np.polyfit(x, y, 1)
+#     plt.plot(x, m * x + b)
+#     plt.show()
+
+
+# df = get_stock_data('AAPL')  # STEP 1
+# datetime_to_closing_prices = get_datetime_to_closing_prices()  # STEP 2
+# monthly_closing_prices = get_monthly_closing_prices(4)  # STEP 3
+# list_of_median_values = calculate_median_values()  # STEP 4
+# list_of_unemployment_rates = get_list_of_unemployment_rates()  # STEP 5
+# x = np.array(list_of_unemployment_rates)
+# y = np.array(list_of_median_values)
+# pprint(get_r_squared())  # STEP 6
+# # plot_the_graph()  # STEP 7
+
+def return_unemployed_per_opening_to_graph(stock_ticker):
+    stock_info = get_stock_data(stock_ticker)
+    datetime_to_closing_prices = get_datetime_to_closing_prices(stock_info)  # STEP 2
+    monthly_closing_prices = get_monthly_closing_prices(4, datetime_to_closing_prices)  # STEP 3
+    list_of_median_values = calculate_median_values(monthly_closing_prices)  # STEP 4
+    list_of_unemployed_per_opening = get_list_of_unemployed_per_opening()  # Step 5 (2)
+    x = np.array(list_of_unemployed_per_opening)
+    y = np.array(list_of_median_values)
+    return x, y
